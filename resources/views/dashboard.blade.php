2618285b5192f4967a6b4793f8647806ac2543a1
octagony <moctagony@gmail.com>
2023-11-26 18:59:15 +0300

    [FEAT] Setup basic blade templates for routes welcome,dashboard,register

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,10 +11,14 @@
 <body>
     <header class="w-full">
         <div class="flex justify-end gap-4 py-2 px-4">
-            <a href="" class="border p-2 bg-red-400 rounded-xl text-white">Login</a>
-            <a href="" class="border p-2 bg-gray-100 rounded-xl text-black">Register</a>
+            <a href="" class="border p-2 bg-red-400 rounded-xl text-white">Log out</a>
         </div>
     </header>
+    <main>
+        <div class="m-6 mb-12 rouded-xl shadow-xl sm:p-10">
+            <h1 class="text-3xl font-semibold">Dashboard</h1>
+        </div>
+    </main>
 </body>
 
 </html>
